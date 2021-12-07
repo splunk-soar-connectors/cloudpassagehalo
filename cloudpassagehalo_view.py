bc0ca0e0814d1131d0e5c5f19e7748df3ffaee16
shiyingtu <85594384+shiyingtu@users.noreply.github.com>
2021-12-06 17:02:26 -0800

    Fix isort linting tests

--- cloudpassagehalo_view.py
+++ cloudpassagehalo_view.py
@@ -12,8 +12,8 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
-import time
 import string
+import time
 
 
 # Function override response
